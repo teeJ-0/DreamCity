a5088075dbbb5cc5355c8bc071c300450218ef7b
teeJ.io <tristenrodriguez1997@gmail.com>
2022-07-31 23:00:35 -0700

    added female clothing & hairstyles

--- txData/CFXDefault_D46D09.base/resources/[esx]/esx_status/config.lua
+++ txData/CFXDefault_D46D09.base/resources/[esx]/esx_status/config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
 Config.StatusMax      = 1000000
-Config.TickTime       = 1000
+Config.TickTime       = 500
 Config.UpdateInterval = 30000
-Config.Display        = true	-- Enable the esx_status bars (disable if you are using another HUD)
+Config.Display        = false	-- Enable the esx_status bars (disable if you are using another HUD)
